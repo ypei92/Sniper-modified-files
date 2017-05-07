c406d00259bd61a621d872f9fcc2507fd8fd901c
ypei <ypei@cs.utexas.edu>
2017-05-07 13:14:22 -0500

    added test headroom2 4:1

---
+++ run_all_hybrid_headroom2/gcc/16/sim.scripts.py
@@ -0,0 +1,3 @@
+import sys
+sys.argv = [ "/v/filer4b/v20q001/ypei/Desktop/cs395t-lin/sniper/scripts/stop-by-icount.py", "250000000" ]
+execfile("/v/filer4b/v20q001/ypei/Desktop/cs395t-lin/sniper/scripts/stop-by-icount.py")
