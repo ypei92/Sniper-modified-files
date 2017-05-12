ffe4a9a8a5892ff6cdac859071d65f8fb565c2e9
ypei <ypei@cs.utexas.edu>
2017-05-12 00:45:55 -0500

    removed outdated data

--- run_all_hybrid_headroom2/omnetpp/21/sim.scripts.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.argv = [ "/v/filer4b/v20q001/ypei/Desktop/cs395t-lin/sniper/scripts/stop-by-icount.py", "250000000" ]
-execfile("/v/filer4b/v20q001/ypei/Desktop/cs395t-lin/sniper/scripts/stop-by-icount.py")
